dcbdd4c4a077955b10c6fe8c99e513b3c98daa97
fajar ghani <fajghanit@gmail.com>
2019-08-13 11:35:09 +0700

    sql

--- db_user.sql
+++ db_user.sql
@@ -11,6 +11,21 @@
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 12/08/2019 10:14:05
+ Date: 13/08/2019 11:32:51
 */
 
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for person
+-- ----------------------------
+DROP TABLE IF EXISTS `person`;
+CREATE TABLE `person`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '0',
+  `last_name` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
